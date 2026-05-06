2ad6a2854124291429c0cf78f410d8807aeb3d2a
Gabriel Botelho <198496875+gBotelho2003@users.noreply.github.com>
2026-05-06 09:51:32 +0200

    forgot to reset the stats in v1

--- V1/Game/reset.js
+++ V1/Game/reset.js
@@ -3,6 +3,8 @@ function resetGame() {
     localStorage.setItem("enableLevel2", "false");
     localStorage.setItem("enableLevel3", "false");
     localStorage.removeItem("playerName");
+    // reset stats list for new user
+    localStorage.removeItem("stats");
     location.reload();
 }
 
